2332ae942589e1226ece9f88f4229a43867fc3dc
2447473295@qq.com <2447473295@qq.com>
2018-04-19 18:08:44 +0800

    NIO

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -2,6 +2,11 @@ package com.example.demo;
 
 import com.jc.tools.CalendarUtil;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -20,9 +25,37 @@ public class Test {
             System.out.println(s);
         }
 
+        NIOTest();
     }
 
 
+    /**
+     * NIO first code
+     * @throws Exception
+     */
+    public static void NIOTest(){
+        System.out.println("==============================NIO==============================");
+        try {
+            RandomAccessFile file = new RandomAccessFile("F:/test.log", "rw");
 
+            FileChannel channel = file.getChannel();
+
+            ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+
+            int res = channel.read(byteBuffer);
+            while (res != -1) {
+                byteBuffer.flip();//从写模式转为读
+                while (byteBuffer.hasRemaining()) {
+                    System.out.print((char) byteBuffer.get());
+                }
+                byteBuffer.clear();
+                res=channel.read(byteBuffer);
+            }
+            file.close();
+        }catch (Exception e){
+            System.out.println(e.getMessage()+e.getCause());
+        }
+
+    }
 
 }
